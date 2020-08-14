dddf8837f2ae5793c878228e6358146e868add70
n3wus3r <trongnghialedinh@gmail.com>
2020-08-14 18:25:57 +0700

    initial configuration

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    migrate.init_app(db, app)
+
+    # attach routes and custom error here
+
+    from app.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
